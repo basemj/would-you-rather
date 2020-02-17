3401cb1501ddd5e4a3bb3a55734dfc6b2b8e8653
Basem Jawahri <basem.jawahri@centrica.com>
2020-02-17 14:25:50 +0000

    feat: plugin store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
+import middleware from './middleware';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(middleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
