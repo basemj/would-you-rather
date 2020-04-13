d1fb773844af66c9bc59ea380a34403819d738f5
Basem Jawahri <3786429+basemj@users.noreply.github.com>
2020-04-13 18:41:19 +0100

    feature: setup app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { handleReceiveQuestions } from './actions/questions';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import AuthedUserInfo from './components/AuthedUserInfo';
+import PrivateRoute from './components/PrivateRoute';
+import PageNotFound from './components/PageNotFound';
+import Navigation from './components/Navigation';
+import NewQuestion from './routes/NewQuestion';
+import Leaderboard from './routes/Leaderboard';
+import Question from './routes/Question';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleReceiveQuestions());
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Navigation />
+        <AuthedUserInfo />
+        <Switch>
+          <PrivateRoute exact path="/">
+            <Home />
+          </PrivateRoute>
+          <PrivateRoute exact path="/new-question">
+            <NewQuestion />
+          </PrivateRoute>
+          <PrivateRoute exact path="/leaderboard">
+            <Leaderboard />
+          </PrivateRoute>
+          <PrivateRoute exact path="/questions/:id">
+            <Question />
+          </PrivateRoute>
+          <Route exact path="/login" component={Login} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(App);
